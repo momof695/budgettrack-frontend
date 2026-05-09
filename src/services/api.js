8572f16ee81ad40823e834162ce2477e190d38fb
Mohamed <sawadogom816@gmail.com>
2026-05-09 14:23:20 +0000

    hardcode production API URL as fallback

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL ?? 'https://budgettrack-api.onrender.com/api',
   headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
 })
 
